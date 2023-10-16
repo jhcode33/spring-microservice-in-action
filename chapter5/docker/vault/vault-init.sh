5a28eb9915316b121ab579bf622c05aabd2287d2
jhcode33 <jhcode33@gmail.com>
2023-10-16 15:04:27 +0900

    Feat: Add Vault Server Docker-Compose
    - CLI를 통해서 Vault 서버가 구성될 때 자동으로 데이터가 작성되게 만들었습니다

--- chapter5/docker/vault/vault-init.sh
+++ chapter5/docker/vault/vault-init.sh
@@ -3,7 +3,7 @@
 echo "==> 스크립트 실행 중"
 echo "==> 서버 dev 모드로 실행"
 # vault server 시작 // -config=/vault/config/config.hcl
-vault server -dev -dev-root-token-id="myroot" -dev-listen-address="0.0.0.0:8200" &
+vault server -dev -dev-root-token-id="myroot" -dev-listen-address="0.0.0.0:8200"&
 
 # 대기 시간 추가 (예: 5초)
 echo "==> 서버가 제대로 동작할 때까지 대기 5초"
@@ -19,6 +19,7 @@ export VAULT_TOKEN='myroot'
 #echo "==> 저장 경로 생성"
 #VAULT_TOKEN=myroot vault secrets enable -version=2 -path=licensing-service kv
 
+# 경로를 새로 생성하는게 잘 안 되서 기존에 있던 secret 경로로 데이터를 생성했습니다
 echo "==> default 데이터 저장"
 VAULT_TOKEN=myroot vault kv put secret/licensing-service/default @/vault/data/default.json
 
